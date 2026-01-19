36f4f6ec8a5c057a0be61650802690e0b9129411
Jeremy K. Johnson <jeremykjohnson@pm.me>
2026-01-18 18:37:47 -0800

    organizing ace cli src

---
+++ src/ace/internal/tooling/tooling.go
@@ -0,0 +1,10 @@
+package tooling
+
+import (
+	_ "github.com/google/uuid"
+	_ "github.com/spf13/cobra"
+	_ "github.com/spf13/pflag"
+	_ "github.com/spf13/viper"
+	_ "github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/mock"
+)
